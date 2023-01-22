bfd0a984ad8bd001d9ec595aad3f40f15bec21a2
darcy-murphy <113056025+darcy-murphy@users.noreply.github.com>
2023-01-22 10:54:47 -0500

    update

--- js/script.js
+++ js/script.js
@@ -5,21 +5,6 @@
 // This file contains the JS functions for index.html
 
 /**
- * Check servie worker.
+ * This function displays "Hello, World!".
+ * 
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Final-Project-Justin-Allen/sw.js", {
-    scope: "/ICS2O-Final-Project-Justin-Allen/",
-  })
-}
-
-;("use strict")
-
-function myButtonClicked() {
-  var c = "Rock";
-  var d = "Paper";
-  var e = "Scissors";
-  var thing = window['cde'.charAt(Math.floor(Math.random()*3))];
-  document.write('The computer chose: ' + thing);
-  
-}
